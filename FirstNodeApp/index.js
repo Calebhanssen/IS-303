60746ac0a0cea855d1b65ac99383e65dd5af519b
Caleb Hanssen <calebhanssen@gmail.com>
2022-03-10 13:32:58 -0700

    wow lots of changes

--- FirstNodeApp/index.js
+++ FirstNodeApp/index.js
@@ -2,4 +2,12 @@ let express = require("express");
 
 let app = express();
 
-app.get("/", (req, res));
+// "/" = root directory
+app.get("/", (req, res) =>
+  res.send("This is my first node express application")
+);
+app.get("/about", (req, res) => res.send("Please contact tech support"));
+app.get("/help", (req, res) => res.send("Please contact tech support"));
+
+app.listen(3000, () => console.log("The serve has started to listen."));
+console.log("Server is running");
