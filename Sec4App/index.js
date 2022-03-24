661a46f1b14275b10289ec4aae122304b5c2c3d9
Caleb Hanssen <calebhanssen@gmail.com>
2022-03-24 13:44:23 -0600

    idk

--- Sec4App/index.js
+++ Sec4App/index.js
@@ -1,8 +1,20 @@
-const { request } = require("express");
 let express = require("express");
 
 let app = express();
 
+let path = require("path");
+
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => res.send("Welcome to my life"));
 
-app.listen(3000, () => console.log("I am listening :)"));
+app.get("/about", (req, res) => res.redirect("https://www.byu.edu"));
+
+app.get("/employee", (req, res) => {
+  console.log("Callling the employee");
+  res.sendFile(path.join(__dirname + "/testdata.html"));
+});
+
+app.listen(port, () => console.log("I am listening :)"));
