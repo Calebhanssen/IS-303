8da7df4781f46ed6883c9d2af2bc074adf7f77e2
Caleb Hanssen <calebhanssen@gmail.com>
2022-03-24 12:19:57 -0600

    why is there so much to save

--- Sec4App/index.js
+++ Sec4App/index.js
@@ -1,1 +1,8 @@
-console.log("Hello");
+const { request } = require("express");
+let express = require("express");
+
+let app = express();
+
+app.get("/", (req, res) => res.send("Welcome to my life"));
+
+app.listen(3000, () => console.log("I am listening :)"));
